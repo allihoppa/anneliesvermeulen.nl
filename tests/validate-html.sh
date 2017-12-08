558de18997fce2cd2315f63c51625e573d5b0b92
Lucas van Lierop <me@lucasvanlierop.nl>
2017-12-08 20:16:20 +0000

    Adds missing html validate script

---
+++ tests/validate-html.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Pull image first so no docker output ends up in stderr later on.
+docker pull magnetikonline/html5validator:latest
+
+assert_valid_html() {
+    local url=$1
+    local validation_errors
+
+
+    # Note that in order to capture the commands stderr its' redirected to stdout
+    # While stdout itself is redirected to a third filedescriptor which is outputted afterwards.
+    { validation_errors=$(
+        docker run \
+        --rm \
+        --name=html-validator \
+        --network=container:anneliesvermeulen-nl-web-ci \
+        magnetikonline/html5validator \
+        java -jar /root/build/validator.nu/vnu.jar $url \
+        2>&1 1>&3-
+    ) ;} 3>&1
+
+    # Catch warnings too
+    if [[ ! -z "${validation_errors}" ]]; then
+        echo "HTML NOT VALID ${validation_errors}"
+        exit 1
+    fi
+}
+
+assert_valid_html 'http://web'
+assert_valid_html 'http://web/contact/'
+assert_valid_html 'http://web/diensten/'
+assert_valid_html 'http://web/over-tekstschrijver-annelies-vermeulen/'
+assert_valid_html 'http://web/projecten/'
